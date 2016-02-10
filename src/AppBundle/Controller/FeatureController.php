11ef709774131603548454ff0e560c142612dfd2
Rederrik <rederrik@gmail.com>
2016-02-11 00:11:39 +0300

    added sample rest resources

--- src/AppBundle/Controller/FeatureController.php
+++ src/AppBundle/Controller/FeatureController.php
@@ -10,11 +10,29 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Feature;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
-class FeatureController extends FOSRestController
+class FeatureController extends FOSRestController implements ClassResourceInterface
 {
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Returns a feature"
+     * )
+     * @param Feature $feature A feature added by user
+     * @return array
+     */
     public function getAction(Feature $feature)
     {
         return ['feature' => $feature];
+    }
+
+    /**
+     * @return array
+     */
+    public function cgetAction()
+    {
+        return ['features' => $this->getDoctrine()->getRepository('AppBundle:Feature')->findAll()];
     }
 }

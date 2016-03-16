bf5b91e83a8b3b06584ca3dcd55d134e4005c7cd
Nooblisk <mitarevigor@gmail.com>
2016-03-16 17:25:14 +0300

    fixed requestDeleteFeature

--- web/js/deleteFeature.js
+++ web/js/deleteFeature.js
@@ -5,21 +5,27 @@
 
 //обработчик, запускающий функцию удаления фичи
 templateColumn.on("click", ".ui.feature.delete.button", function () {
-    var featureOrder = $(this).parents(".tab.container").data("order");
-    console.log(featureOrder);
+   // var featureOrder = $(this).parents(".tab.container").data("order");
+    //console.log(featureOrder);
     if (window.confirm()) {
-        requestDeleteFeature(this.dataset.id, featureOrder);
+        requestDeleteFeature(this.dataset.id);
     }
 });
 
 //удаляет фичу по id
-var requestDeleteFeature = function (feature, featureOrder) {
+var requestDeleteFeature = function (feature) {
         apiClient.deleteFeature(feature).success(function () {
             spiner.down();
             statusBar();
-            $(".item.feature").eq(featureOrder).remove();
-            $(".tab.container").eq(featureOrder).remove();
-            $(".item.feature").eq(featureOrder-1).click();
+            var index = $("#itemFeature"+feature).index();
+            if (index > 0) {
+                $("#itemFeature" + feature).prev().click();
+            }
+            else{
+                $('.item.feature').eq(index+1).click();
+            }
+            $("#itemFeature"+feature).remove();
+            $("#containerFeature"+feature).remove();
             localStorage.removeItem("Quest"+feature);
             synchronizeFeatures();
         }).fail(function(xhr){

beb023ff2003026c4d59c6bd2f3468b08d69edd6
Nooblisk <mitarevigor@gmail.com>
2016-03-20 01:27:11 +0300

    road to reactive updated userDataFunctions.js added some icons

--- web/js/UserDataFunctions.js
+++ web/js/UserDataFunctions.js
@@ -2,44 +2,43 @@
  * Created by Nooblisk on 05.03.2016.
  */
 
-//$("#buttonUserInfo").click(function(){
-//    $("#modalUserInfo").modal({
-//            onApprove: function () {
-//                localStorage.clear();
-//                //templateList.empty();
-//                //templateColumn.empty();
-//                location.reload();
-//            }
-//        })
-//        .modal('show')
-//    ;
-//});
 $("#buttonLogOut").click(function() {
     localStorage.clear();
     location.reload();
 });
 
-
-//забирает данные пользователя и отображает
-var synchronizeUserInfo = function () {
+//забирает данные пользователя, записывает их в объект и заполняет поля данными
+var synchronizeUserInfoAndFill = function () {
         apiClient.getUser().success(function (UserInfo) {
             spiner.down();
             statusBar();
             apiClient.setUserInfo(UserInfo);
             localStorage.setItem('UserInfo', JSON.stringify(apiClient.UserInfo()));
-            userInfoFill();
+            userInfoFill(apiClient.UserInfo());
         }).fail(function(xhr){
             apiClient.authFail(xhr, synchronizeUserInfo);
         })
     }
     ;
 
-//отображает данные пользователя
-var userInfoFill = function(){
-    var UserInfo = apiClient.UserInfo();
+//забирает данные пользователя и записывает их в объект
+var synchronizeUserInfo = function () {
+        apiClient.getUser().success(function (UserInfo) {
+            spiner.down();
+            statusBar();
+            apiClient.setUserInfo(UserInfo);
+            localStorage.setItem('UserInfo', JSON.stringify(apiClient.UserInfo()));
+            reactiveUserInfo.set(UserInfo);
+        }).fail(function(xhr){
+            apiClient.authFail(xhr, synchronizeUserInfo);
+        })
+    }
+    ;
+
+//Заполняет поля данными
+var userInfoFill = function(UserInfo){
     $("#userUsername").text("Пользователь "+UserInfo.user.username);
     $("#userLevel").text("Уровень " + UserInfo.user.level);
     $("#userEmail").text("E-mail "+UserInfo.user.email);
-   // $("#buttonUserInfo").text(UserInfo.user.username+" "+UserInfo.user.level);
 };
 
